17555404b7686fc94729bf7de5aa1ead4b29e909
Julian Kohlman <juliankohlman@gmail.com>
2018-09-17 10:01:37 -0400

    get current profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load Profile/User Models
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 /**
   @route   GET api/profile/test
@@ -10,5 +16,28 @@ const router = express.Router();
 router.get('/test', (req, res) => {
 	res.json({ msg: 'Profile route working' });
 });
+
+/**
+  @route   GET api/profile
+  @desc    Get current users profile
+  @access  Private
+*/
+
+router.get(
+	'/',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		const errors = {};
+		Profile.findOne({ user: req.user.id })
+			.then(profile => {
+				if (!profile) {
+					errors.noprofile = 'Profile for user does not exist.';
+					return res.status(404).json(errors);
+				}
+				res.json(profile);
+			})
+			.catch(err => res.status(404).json(err));
+	}
+);
 
 module.exports = router;
