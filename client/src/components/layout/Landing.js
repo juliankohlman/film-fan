a493606c1c4dfde62b881730a8ef5b856f5f24ff
Julian Kohlman <juliankohlman@gmail.com>
2018-09-19 10:47:34 -0400

    Adding state to registration form

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Landing extends Component {
 	render() {
@@ -15,18 +16,18 @@ class Landing extends Component {
 									connected with other fans.
 								</p>
 								<hr />
-								<a
-									href="register.html"
+								<Link
+									to="/register"
 									className="btn btn-lg btn-info mr-2"
 								>
 									Sign Up
-								</a>
-								<a
-									href="login.html"
+								</Link>
+								<Link
+									to="/login"
 									className="btn btn-lg btn-light"
 								>
 									Login
-								</a>
+								</Link>
 							</div>
 						</div>
 					</div>
