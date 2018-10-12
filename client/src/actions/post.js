9f1bea4298c0b2961915a5ac0b01a1bc29f481a9
Julian Kohlman <juliankohlman@gmail.com>
2018-10-12 10:13:34 -0400

    Create addPost action/type

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,9 +5,24 @@ import {
 	POST_LOADING,
 	GET_POSTS,
 	GET_POST,
-	ADD_POSTS,
+	ADD_POST,
 	DELETE_POST
 } from './types';
 
 // Add a post
-export const addPost = () => {};
+export const addPost = postData => dispatch => {
+	axios
+		.post('/api/posts', postData)
+		.then(res => {
+			dispatch({
+				type: ADD_POST,
+				payload: res.data
+			});
+		})
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
