28f3af20e47856a0de606269a5044c7f0c4842d9
Julian Kohlman <juliankohlman@gmail.com>
2018-12-04 19:36:52 -0500

    debug heroku build failure

--- server.js
+++ server.js
@@ -9,24 +9,8 @@ const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
 
 const app = express();
-const port = process.env.PORT || 5000;
-
 // DB
 const db = require('./config/keys').mongoURI;
-
-// Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(passport.initialize());
-
-// Serve static assets in prod env
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
 
 // Passport config access (strategies)
 require('./config/passport')(passport);
@@ -37,14 +21,29 @@ mongoose
 	.then(() => console.log('Now Connected'))
 	.catch(err => console.log(err));
 
-// Test route
-// app.get('/', (req, res) => {
-// 	res.send('Hello from Test route');
-// });
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
 
 // Use Routes
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
+
+// Serve static assets in prod env
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+const port = process.env.PORT || 5000;
+// Test route
+// app.get('/', (req, res) => {
+// 	res.send('Hello from Test route');
+// });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
